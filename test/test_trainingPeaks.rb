56f94166b7311d1ec134e147cc9eaf53ec64fd3f
Scott Francis <scott.russell.francis@gmail.com>
2014-10-24 15:49:55 -0700

    building up test suite

--- test/test_trainingPeaks.rb
+++ test/test_trainingPeaks.rb
@@ -2,11 +2,12 @@
 
 tp= TrainingPeaks.new
 
-isGood= tp.authenticateAccount('scottrfrancis','66Runn!!')
+isGood= tp.authenticateAccount('user','pass')
 puts( "account is good " + isGood.to_s )
 
 workouts= tp.getWorkouts('2014-10-17','2014-10-24')
-puts( "got workouts: " + workouts.to_s )
+#puts( "got workouts: " + workouts.to_s )
+workouts.map{|w| puts( w[:workout_id] + " " + w[:workout_day].to_s + " " + w[:workout_type_description] + " " + w[:time_total_in_seconds].to_s )}
 
 tp.saveWorkoutDataToFile( workouts.last[:workout_id], "test.pwx" )
 puts("check test.pwx")
