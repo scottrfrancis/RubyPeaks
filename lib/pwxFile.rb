25b6c9c5960e1a0209ffbb092cdddaa45812088b
Scott Francis <scott.russell.francis@gmail.com>
2014-11-04 16:26:59 -0800

    now gets summary data from pwxfile

--- lib/pwxFile.rb
+++ lib/pwxFile.rb
@@ -14,7 +14,7 @@ class PWXFile
   def loadFile( aFileName )
     if !aFileName.nil? && aFileName != ""
       File.open( aFileName ) do |f|
-        @doc = Nokogiri::XML( f ).remove_namespaces!
+        @doc = Nokogiri::XML( f )
       end
     end
     
@@ -22,8 +22,26 @@ class PWXFile
   end
   
   def getSummary()
+    summary = {}
     
-    
+    if !@doc.nil?
+      workoutSummary = @doc.xpath( "//xmlns:workout/xmlns:summarydata" )
+      
+      workoutSummary.children.each do |n|
+        next if n.class != Nokogiri::XML::Element
+        
+        if n.attributes.length <= 0
+          summary[n.name] = n.text.to_f
+        else
+          summary[n.name] = {}
+          n.attributes.each do |k,v|
+            summary[n.name][k] = v.value.to_f
+          end
+        end
+      end
+    end
+      
+    summary
   end
   
 end
